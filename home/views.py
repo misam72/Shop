4c2940e6a184044d078d37873dc0205bb40c1882
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-23 20:39:02 +0330

    Implement manual storages 2: buckets, list all of objects and a view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Product
+from .tasks import all_bucket_objects_task
 
 class HomeView(View):
     def get(self, request):
@@ -12,3 +13,11 @@ class ProductDetailView(View):
         product = get_object_or_404(Product, slug=slug)
         print(product.image.url, '***')
         return render(request, 'home/detail.html', {'product':product})
+
+class BucketHome(View):
+    template_name = 'home/bucket.html'
+    
+    def get(self, request):
+        objects = all_bucket_objects_task()
+        # print(objects)
+        return render(request, self.template_name, {'objects': objects})
