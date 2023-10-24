93eb1b772a8397f73df2591c256f8ce0488b27a4
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-24 15:40:19 +0330

    Fixing timezone problem

--- account/management/commands/remove_expired_otps.py
+++ account/management/commands/remove_expired_otps.py
@@ -2,13 +2,14 @@ from typing import Any
 from django.core.management.base import BaseCommand
 from account.models import OtpCode
 from datetime import datetime, timedelta
+import pytz
 
 
 class Command(BaseCommand):
     help = 'Remove all expired codes'
     
     def handle(self, *args: Any, **options: Any):
-        expired_time = datetime.now() - timedelta(minutes=2)
+        expired_time = datetime.now(tz=pytz.timezone("Asia/Tehran")) - timedelta(minutes=2)
         # Remove the codes that their lifetime is bigger than 2 minutes.
         OtpCode.objects.filter(created__lt=expired_time).delete()
         # We can also use styles...
