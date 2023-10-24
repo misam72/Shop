aec12f9a24f758f964045209b60aaef8f749da84
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-24 14:52:56 +0330

    Implement remove expired codes by management commands.

---
+++ account/management/commands/remove_expired_otps.py
@@ -0,0 +1,16 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+from account.models import OtpCode
+from datetime import datetime, timedelta
+
+
+class Command(BaseCommand):
+    help = 'Remove all expired codes'
+    
+    def handle(self, *args: Any, **options: Any):
+        expired_time = datetime.now() - timedelta(minutes=2)
+        # Remove the codes that their lifetime is bigger than 2 minutes.
+        OtpCode.objects.filter(created__lt=expired_time).delete()
+        # We can also use styles...
+        self.stdout.write('All expired otp codes removed.')
+        
