1228cd8f72acdc68e1c031cdd19cec9154786f39
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-29 15:17:19 +0330

    3 Ways of how to use the User model in a model.

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Order(models.Model):
+    # For using the User model as foreign key we have 3 options:
+    # Opt 1:
+    # from account.models import User
+    # user = models.ForeignKey(User,...)
+    # Opt 2:
+    # from django.contrib.auth import get_user_model
+    # user = models.ForeignKey(get_user_model(),...)
+    # Opt 3:
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class OrderItem(models.Model):
+    ...
